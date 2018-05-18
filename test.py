aeba5cbfe25a1a7ce32e0964abfc10155b87e716
zhaohao.chen <cartkbackville@gmail.com>
2018-05-18 12:38:51 +0800

    def

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import xlrd
-data = xlrd.open_workbook("C:\\Users\\Heymax\\Desktop\\DBTest.xlsx")
-table = data.sheets()[0]
+data = xlrd.open_workbook("C:\\Users\\80649\\Desktop\\QQ-QA-2018-5-3.xlsx")
+#table = data.sheets()[0]
+def print_sheet(sheet):
+	for row in range(0,sheet.nrows):
+		temp=""
+		for col in range(0,sheet.ncols):
+			if sheet.cell(row,col).value != None:
+				temp += str(sheet.cell(row,col).value)+"\t"
+		print(temp)
+		
+def main():
+	for sheet in data.sheets():
+		print_sheet(sheet)
 
-for row in range(0,table.nrows):
-    temp=""
-    for col in range(0,table.ncols):
-        if table.cell(row,col).value != None:
-            temp += str(table.cell(row,col).value)+"\t"
-    print(temp)
-
+if __name__ == "__main__":
+	main()
